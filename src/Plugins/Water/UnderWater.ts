9fa55bf5a3c639cad93e7d1aa1ca9cdae55be1a3
martinRenou <martin.renou@gmail.com>
2020-06-15 14:38:39 +0200

    Compute env normals in shaders
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- src/Plugins/Water/UnderWater.ts
+++ src/Plugins/Water/UnderWater.ts
@@ -42,24 +42,17 @@ void main() {
 
 // Environment shaders
 const envVertex = `
-uniform vec3 light;
-uniform bool revertNormal;
-
 // Light projection matrix
 uniform mat4 lightProjectionMatrix;
 uniform mat4 lightViewMatrix;
 
-varying float lightIntensity;
 varying vec3 lightPosition;
+varying vec3 worldPosition;
 
 
 void main(void){
-  vec3 norm = normal;
-  if (revertNormal) {
-    norm = - norm;
-  }
-
-  lightIntensity = - dot(light, normalize(norm));
+  vec4 modelPosition = modelMatrix * vec4(position, 1.);
+  worldPosition = modelPosition.xyz;
 
   // Compute position in the light coordinates system, this will be used for
   // comparing fragment depth with the caustics texture
@@ -67,15 +60,16 @@ void main(void){
   lightPosition = 0.5 + lightRelativePosition.xyz / lightRelativePosition.w * 0.5;
 
   // The position of the vertex
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+  gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
 `;
 
 const envFragment = `
+uniform vec3 light;
 uniform sampler2D caustics;
 
-varying float lightIntensity;
 varying vec3 lightPosition;
+varying vec3 worldPosition;
 
 const float bias = 0.005;
 
@@ -83,6 +77,13 @@ const vec3 underwaterColor = vec3(0.4, 0.9, 1.0);
 
 
 void main() {
+  // Compute flat shading normal (inefficient, should be computed on CPU once)
+  vec3 X = dFdx(worldPosition);
+  vec3 Y = dFdy(worldPosition);
+  vec3 normal = normalize(cross(X, Y));
+
+  float lightIntensity = - dot(light, normalize(normal));
+
   // Set the frag color
   float computedLightIntensity = 0.5;
 
@@ -126,18 +127,18 @@ class UnderWater extends Effect {
       fragmentShader: envFragment,
       uniforms: {
         light: { value: null },
-        revertNormal: { value: false },
         caustics: { value: null },
         lightProjectionMatrix: { value: null },
         lightViewMatrix: { value: null }
       },
+      extensions: {
+        derivatives: true
+      }
     });
 
     this.envMappingMeshes = [];
     this.envMeshes = [];
     for (const mesh of parent.meshes) {
-      mesh.geometry.computeVertexNormals();
-
       const envMappingMesh = new THREE.Mesh(mesh.geometry, this.envMappingMaterial)
       envMappingMesh.matrixAutoUpdate = false;
       this.envMappingMeshes.push(envMappingMesh);
@@ -167,17 +168,6 @@ class UnderWater extends Effect {
 
   setCausticsTexture (causticsTexture: THREE.Texture) {
     this.envMaterial.uniforms['caustics'].value = causticsTexture;
-  }
-
-  set side (side: number) {
-    if (side === THREE.BackSide) {
-      this.envMaterial.uniforms['revertNormal'].value = true;
-    } else {
-      this.envMaterial.uniforms['revertNormal'].value = false;
-    }
-
-    this.envMappingMaterial.side = side;
-    this.envMaterial.side = side;
   }
 
   renderEnvMap (renderer: THREE.WebGLRenderer, lightCamera: THREE.Camera) {
