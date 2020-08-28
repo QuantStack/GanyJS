62836f553a155f7b3832343b5898a24f2e1cd0e1
martinRenou <martin.renou@gmail.com>
2020-08-28 16:55:09 +0200

    Add mask property to the basicnodematerial
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- src/utils/BasicNode.ts
+++ src/utils/BasicNode.ts
@@ -72,11 +72,15 @@ class BasicNode extends Nodes.Node {
       code = output.join("\n");
     } else {
       // Analyze all nodes to reuse generate codes
+      if ( this.mask ) this.mask.analyze( builder );
+
       this.color.analyze(builder, { slot: 'color' });
 
       if (this.alpha) this.alpha.analyze( builder );
 
       // Build code
+      // @ts-ignore: https://github.com/mrdoob/three.js/pull/19896
+      const mask: Flow = this.mask ? this.mask.flow( builder, 'b' ) : undefined;
       // @ts-ignore: https://github.com/mrdoob/three.js/pull/19896
       const color: Flow = this.color.flow(builder, 'c', { slot: 'color' });
       // @ts-ignore: https://github.com/mrdoob/three.js/pull/19896
@@ -100,6 +104,13 @@ class BasicNode extends Nodes.Node {
         // add before: prevent undeclared normal
         " #include <normal_fragment_begin>",
       ];
+
+      if ( mask ) {
+        output.push(
+          mask.code,
+          'if ( ! ' + mask.result + ' ) discard;'
+        );
+      }
 
       output.push(color.code);
 
@@ -129,97 +140,8 @@ class BasicNode extends Nodes.Node {
   alpha: Nodes.Node | null;
   position: Nodes.Node | null;
   color: Nodes.Node;
+  mask: Nodes.Node | null;
 
   static nodeType: string = 'Basic';
 
 }
-
-// StandardNode.prototype.copy = function ( source ) {
-
-//   Node.prototype.copy.call( this, source );
-
-//   // vertex
-
-//   if ( source.position ) this.position = source.position;
-
-//   // fragment
-
-//   this.color = source.color;
-//   this.roughness = source.roughness;
-//   this.metalness = source.metalness;
-
-//   if ( source.mask ) this.mask = source.mask;
-
-//   if ( source.alpha ) this.alpha = source.alpha;
-
-//   if ( source.normal ) this.normal = source.normal;
-
-//   if ( source.clearcoat ) this.clearcoat = source.clearcoat;
-//   if ( source.clearcoatRoughness ) this.clearcoatRoughness = source.clearcoatRoughness;
-//   if ( source.clearcoatNormal ) this.clearcoatNormal = source.clearcoatNormal;
-
-//   if ( source.reflectivity ) this.reflectivity = source.reflectivity;
-
-//   if ( source.light ) this.light = source.light;
-//   if ( source.shadow ) this.shadow = source.shadow;
-
-//   if ( source.ao ) this.ao = source.ao;
-
-//   if ( source.emissive ) this.emissive = source.emissive;
-//   if ( source.ambient ) this.ambient = source.ambient;
-
-//   if ( source.environment ) this.environment = source.environment;
-
-//   if ( source.sheen ) this.sheen = source.sheen;
-
-//   return this;
-
-// };
-
-// StandardNode.prototype.toJSON = function ( meta ) {
-
-//   var data = this.getJSONNode( meta );
-
-//   if ( ! data ) {
-
-//     data = this.createJSONNode( meta );
-
-//     // vertex
-
-//     if ( this.position ) data.position = this.position.toJSON( meta ).uuid;
-
-//     // fragment
-
-//     data.color = this.color.toJSON( meta ).uuid;
-//     data.roughness = this.roughness.toJSON( meta ).uuid;
-//     data.metalness = this.metalness.toJSON( meta ).uuid;
-
-//     if ( this.mask ) data.mask = this.mask.toJSON( meta ).uuid;
-
-//     if ( this.alpha ) data.alpha = this.alpha.toJSON( meta ).uuid;
-
-//     if ( this.normal ) data.normal = this.normal.toJSON( meta ).uuid;
-
-//     if ( this.clearcoat ) data.clearcoat = this.clearcoat.toJSON( meta ).uuid;
-//     if ( this.clearcoatRoughness ) data.clearcoatRoughness = this.clearcoatRoughness.toJSON( meta ).uuid;
-//     if ( this.clearcoatNormal ) data.clearcoatNormal = this.clearcoatNormal.toJSON( meta ).uuid;
-
-//     if ( this.reflectivity ) data.reflectivity = this.reflectivity.toJSON( meta ).uuid;
-
-//     if ( this.light ) data.light = this.light.toJSON( meta ).uuid;
-//     if ( this.shadow ) data.shadow = this.shadow.toJSON( meta ).uuid;
-
-//     if ( this.ao ) data.ao = this.ao.toJSON( meta ).uuid;
-
-//     if ( this.emissive ) data.emissive = this.emissive.toJSON( meta ).uuid;
-//     if ( this.ambient ) data.ambient = this.ambient.toJSON( meta ).uuid;
-
-//     if ( this.environment ) data.environment = this.environment.toJSON( meta ).uuid;
-
-//     if ( this.sheen ) data.sheen = this.sheen.toJSON( meta ).uuid;
-
-//   }
-
-//   return data;
-
-// };
